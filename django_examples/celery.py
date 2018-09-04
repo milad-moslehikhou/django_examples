a33a2f7adefd5f7fe8fdad4cad89bb0e04ad80eb
miladmoslehi <milad.bu@gmail.com>
2018-09-05 03:40:46 +0430

    fix celery DisabledBackEnd

--- django_examples/celery.py
+++ django_examples/celery.py
@@ -3,6 +3,6 @@ import os
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_examples.settings')
-app = Celery('django_examples')
+app = Celery('django_examples', broker='amqp://celery:celery@localhost//', backend='amqp')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
